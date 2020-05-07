e13d7ae425fc7af4700c3621221005352fa2d0b9
kartikeya sharma <karthiksharma1411@gmail.com>
2020-05-07 15:36:05 +0530

    first commit

--- lib/joke.dart
+++ lib/joke.dart
@@ -14,6 +14,13 @@ class Joke extends StatefulWidget
 
 class _JokeState extends State<Joke>
 {
+
+    Future<Null> refreshList() async{
+        await Future.delayed(Duration(seconds: 2));
+        setState(() {
+        });
+        return null;
+    }
   @override
   Widget build(BuildContext context)
   {
@@ -22,36 +29,132 @@ class _JokeState extends State<Joke>
               title:new Text("Aaaaaa",style:GoogleFonts.shadowsIntoLight(fontSize: 45,fontStyle:FontStyle.normal )) ,
               centerTitle: true,
           ),
-          body:Container(
-              padding: const EdgeInsets.only(top:20,bottom:100,left:80,right: 80),
-            child: Column(
-              children: <Widget>[
-                FutureBuilder
-                    (
-                    future: getJoke(),
-                    builder: (BuildContext context, AsyncSnapshot snapshot)
-                    {
-                        if (snapshot.data == null)
-                        {
-                            return Container(
-                                padding: const EdgeInsets.only(top:10,bottom:100,left:80,right: 80),
-                              child: SpinKitRing(
-                                  size: 40,
-                                  color: Colors.white,
-                              ),
-                            );
-                        }
-                        else
-                        {
-                            return hy("${snapshot.data}");
-                            }
-                    }
-                ),
-              ],
+            drawer: new Drawer(
+            child:new ListView(
+                children: <Widget>[
+                    new ListTile(
+                        title: Center(child: Text("If you want to change name",style:GoogleFonts.shadowsIntoLight(fontSize: 22,fontStyle:FontStyle.normal,fontWeight: FontWeight.bold ),)),
+                    ),
+                    new ListTile(
+                       title: Center(child: Text("Bitch Please",style:GoogleFonts.shadowsIntoLight(fontSize: 30,fontStyle:FontStyle.normal,fontWeight: FontWeight.bold ),)),
+                    ),
+                    new ListTile(
+                        title: new Column(
+                            children: <Widget>[
+                                new TextFormField(
+                                    controller: t3,
+                                    decoration: new InputDecoration(
+                                        hintText: "First Name",
+                                    ),
+                                    keyboardType: TextInputType.text,
+                                ),
+                                Padding(
+                                    padding: const EdgeInsets.only(top:30),
+                                ),
+                                new TextFormField(
+                                    controller: t4,
+                                    decoration: new InputDecoration(
+                                        hintText: "Last Name"
+                                    ),
+                                    keyboardType: TextInputType.text,
+                                ),
+                                Padding(padding: const EdgeInsets.only(top:20)),
+                                MaterialButton(child: new Text("Aww",style:GoogleFonts.shadowsIntoLight(fontSize: 25,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold,color: Colors.white ) ),
+                                onPressed:() {
+                                  two();
+                                },)
+                            ],
+                        ),
+                    ),
+
+                    new ListTile(
+                        title: new Text("close",style:GoogleFonts.shadowsIntoLight(fontSize: 22,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold,color: Colors.white )),
+                        trailing: new Icon(Icons.close),
+                        onTap:()=> Navigator.of(context).pop(),
+                    ),
+
+                ],
+            )
             ),
-          )
+            body: RefreshIndicator(
+                backgroundColor: Colors.grey[700],
+                onRefresh: refreshList,
+              child: ListView.builder(itemCount: 1,
+                  itemBuilder: (context,i)=>
+                   Container(
+                    padding: const EdgeInsets.only(top:20,bottom:100,left:80,right: 80),
+                  child: Column(
+                    children: <Widget>[
+                      FutureBuilder
+                          (
+                          future: getJoke(),
+                          builder: (BuildContext context, AsyncSnapshot snapshot)
+                          {
+                              if (snapshot.data == null)
+                              {
+                                  return Container(
+                                      padding: const EdgeInsets.only(top:150,bottom:100,left:80,right: 80),
+                                    child: SpinKitRing(
+                                        size: 40,
+                                        color: Colors.white,
+                                    ),
+                                  );
+                              }
+                              else
+                              {
+                                  return hy("${snapshot.data}");
+                                  }
+                          }
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
       );
   }
+
+
+    void two(){
+        setState(() {
+            if  (t3.text!="" && t4.text!="" || t3.text=="" && t4.text!=""|| t3.text!="" && t4.text=="") {
+                Navigator.of(context).pop();
+                Navigator.of(context).push
+                    (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));}
+            else{
+                showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (BuildContext context) {
+                        return AlertDialog(
+                            backgroundColor: Colors.grey[900],
+                            title: Text(" If you Enter Nothing You will get Chandler Bing's Jokes  ",style:GoogleFonts.shadowsIntoLight(fontSize: 22)),
+                            actions: <Widget>[
+                                FlatButton(
+                                    child: Text('Back',style:GoogleFonts.shadowsIntoLight(fontSize: 30,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold )),color: Colors.grey[700],
+                                    onPressed: () {
+                                        Navigator.of(context).pop();
+                                    },
+                                ),
+                                Padding(padding:EdgeInsets.only(right:70,bottom:30),)
+                                ,FlatButton(
+                                    child: Text('Okay',style:GoogleFonts.shadowsIntoLight(fontSize: 30,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold )),color: Colors.grey[700],
+                                    onPressed: () {
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context).push
+                                            (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));
+
+                                    },
+                                ),
+                                Padding(padding:EdgeInsets.only(right:30,bottom: 30),)
+                            ],
+                        );
+                    });
+            }
+        });
+
+    }
+
 }
 Center hy(String s)
 {
@@ -68,7 +171,6 @@ Center hy(String s)
           ),
     );
 }
-
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,8 @@ class MyApp extends StatelessWidget
 }
 final TextEditingController t1=new TextEditingController(text:"");
 final TextEditingController t2=new TextEditingController(text:"");
+final TextEditingController t3=new TextEditingController(text:"");
+final TextEditingController t4=new TextEditingController(text:"");
 String p=t1.text;
 String q=t2.text;
 
@@ -67,8 +69,11 @@ class _HomePageState extends State<HomePage>
                             ),
                             Padding(padding: const EdgeInsets.only(top:20)),
                         MaterialButton(child: new Text("Aww",style:GoogleFonts.shadowsIntoLight(fontSize: 40,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold ) ),onPressed: (){
-                            Navigator.of(context).push
-                            (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));}
+//                            Navigator.of(context).pop();
+//                            Navigator.of(context).push
+//                            (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));
+                        one();
+                          }
 
                         ,),
                             ],
@@ -88,32 +93,94 @@ class _HomePageState extends State<HomePage>
         });
 
     }
+    void one(){
+        setState(() {
+            if  (t1.text!="" && t2.text!="" || t1.text!="" && t2.text=="" || t1.text=="" && t2.text!="") {
+            Navigator.of(context).pop();
+            Navigator.of(context).push
+                (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));}
+            else{
+                    showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (BuildContext context) {
+                            return AlertDialog(
+                                backgroundColor: Colors.grey[900],
+                                title: Text(" If you Enter Nothing You will get Chandler Bing's Jokes  ",style:GoogleFonts.shadowsIntoLight(fontSize: 22)),
+                                actions: <Widget>[
+                                    FlatButton(
+                                    child: Text('Back',style:GoogleFonts.shadowsIntoLight(fontSize: 30,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold )),color: Colors.grey[700],
+                                    onPressed: () {
+                                        _cle();
+                                        Navigator.of(context).pop();
+                                    },
+                                ),
+                                    Padding(padding:EdgeInsets.only(right:70,bottom:30),)
+                                    ,FlatButton(
+                                        child: Text('Okay',style:GoogleFonts.shadowsIntoLight(fontSize: 30,fontStyle:FontStyle.italic,fontWeight: FontWeight.bold )),color: Colors.grey[700],
+                                        onPressed: () {
+                                            _cle();
+                                            Navigator.of(context).pop();
+                                            Navigator.of(context).push
+                                                (new MaterialPageRoute(builder:(BuildContext context)=> Joke()));
+
+                                        },
+                                    ),
+                                    Padding(padding:EdgeInsets.only(right:30,bottom: 30),)
+                                ],
+                            );
+                        });
+                }
+        });
+
+    }
 
 
 }
+
+
+
+
+
 Future getJoke()
 async {
 
     var data = await http.get(
         "http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe1http://api.icndb.com/jokes/random?firstName=John&amp;lastName=Doe");
     var jsonData = json.decode(data.body);
-    if (p!="" && q!=""
-    ) {
-        String str = jsonData["value"]["joke"];
-        String l = str.replaceAll("&quot;", '"');
-        String b = l.replaceAll("&quot;.", '"');
-        String m = b.replaceAll("John", p);
-        String k = m.replaceAll("Norris", q);
-        return k;
+    String str = jsonData["value"]["joke"];
+    String l = str.replaceAll("&quot;", '"');
+    String b = l.replaceAll("&quot;.", '"');
+
+    if ((p!="" && q!="" || p!="" && q=="" ||  p=="" && q!="") && t3.text=="" && t4.text=="") {
+        if (p == t1.text && q == t2.text) {
+            String m = b.replaceAll("John", p);
+            String k = m.replaceAll("Norris", q);
+            return k;
+        }
+        else
+        {
+            String m = b.replaceAll("John","Chandler");
+            String k = m.replaceAll("Norris","Bing" );
+            return k;
+        }
     }
-    else {
-        String str = jsonData["value"]["joke"];
-        String l = str.replaceAll("&quot;", '"');
-        String b = l.replaceAll("&quot;.", '"');
+    else if(p=="" && q=="" && t3.text=="" && t4.text=="")
+    {
         String m = b.replaceAll("John","Chandler" );
         String k = m.replaceAll("Norris", "Bing");
         return k;
     }
+    else if(t3.text!="" && t4.text!="" ||t3.text!="" && t4.text==""|| t3.text!="" && t4.text=="")
+        {
+            p=t3.text;
+            q=t4.text;
+
+            String m = b.replaceAll("John",p );
+            String k = m.replaceAll("Norris",q);
+            return k;
+        }
+
 
 }
 
